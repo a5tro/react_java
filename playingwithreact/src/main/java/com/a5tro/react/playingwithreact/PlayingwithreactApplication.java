aaaa966b0355faa1fe8c1dfc9b33d95dc29b4414
a5tro <40609028+a5tro@users.noreply.github.com>
2023-09-29 01:06:22 +0000

    initial commit

---
+++ playingwithreact/src/main/java/com/a5tro/react/playingwithreact/PlayingwithreactApplication.java
@@ -0,0 +1,13 @@
+package com.a5tro.react.playingwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingwithreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingwithreactApplication.class, args);
+	}
+
+}
---
+++ playingwithreact/src/test/java/com/a5tro/react/playingwithreact/PlayingwithreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a5tro.react.playingwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingwithreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
